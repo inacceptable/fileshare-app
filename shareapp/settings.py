553429ffa723b4fc714fd206bc2d178bc68fc016
Con <62197403+inacceptable@users.noreply.github.com>
2022-06-07 15:08:49 +1000

    Update settings.py

--- shareapp/settings.py
+++ shareapp/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
